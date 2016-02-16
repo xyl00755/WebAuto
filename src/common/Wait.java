8d6fb7350ff14cd5f99eb1f2d47fc2491a9b09f7
liyafei <liyafei@danlu.com>
2016-02-16 16:46:50 +0800

    change param from object to string

--- src/common/Wait.java
+++ src/common/Wait.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 
 public class Wait {
 
-	public static void run(HashMap<String, Object> params){
+	public static void run(HashMap<String, String> params){
 	    if(params.get("Seconds") != null){
 	    	try {
-				Thread.sleep(Integer.parseInt((String) params.get("Seconds")) * 1000);
+				Thread.sleep(Integer.parseInt(params.get("Seconds")) * 1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
