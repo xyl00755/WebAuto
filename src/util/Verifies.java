44473202808f73156476e4687f589549a3c71e87
liyafei <liyafei@danlu.com>
2016-02-18 14:36:30 +0800

    add

---
+++ src/util/Verifies.java
@@ -0,0 +1,27 @@
+package util;
+
+import common.Elements;
+import util.Browser;
+
+import java.util.HashMap;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class Verifies {
+	
+	public static void attribute(HashMap<String, String> params){
+		WebElement element = Elements.find(params,Browser.Driver);
+		Assert.assertEquals(element.getAttribute(params.get("Attribute Name")),params.get("Value"));
+	}
+	
+	public static void pagetitle(HashMap<String, String> params){
+		Assert.assertEquals(Browser.Driver.getTitle(),params.get("Title"));
+	}
+	
+	public static void text(HashMap<String, String> params){
+		WebElement element = Elements.find(params,Browser.Driver);
+		Assert.assertEquals(element.getText(),params.get("Text"));
+	}
+	
+}
