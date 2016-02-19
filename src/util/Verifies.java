ab3941f9676a78b1bf4c51f152666bb014d5f13f
liyafei <liyafei@danlu.com>
2016-02-19 10:14:13 +0800

    verify check box is checked or not

--- src/util/Verifies.java
+++ src/util/Verifies.java
@@ -27,4 +27,9 @@ public class Verifies {
     public static void currenturl(HashMap<String, String> params){
 		Assert.assertEquals(Browser.Driver.getCurrentUrl(),params.get("Value"));
 	}
+   //CheckBox
+	public static void checkbox(HashMap<String, String> params){
+		WebElement element = Elements.find(params,Browser.Driver);
+		Assert.assertEquals(element.isSelected(),params.get("Is Checked").equals("true"));
+	}
 }
