66c2a4919b709545d156cf220bbc3646414eba4e
chenshu <chenshu@danlu.com>
2016-02-25 19:18:57 +0800

    add getCookies

--- src/util/DeleteCookie.java
+++ src/util/DeleteCookie.java
@@ -6,9 +6,13 @@ public class DeleteCookie {
 	
 	public static void run(HashMap<String, String> params){
 		if(params.get("Cookie Name") == "ALL"){
+			System.out.println("Cookies:"+Browser.Driver.manage().getCookies());
 			Browser.Driver.manage().deleteAllCookies();
+			System.out.println("Cookies:"+Browser.Driver.manage().getCookies());
 		} else {
+			System.out.println("Cookies:"+Browser.Driver.manage().getCookies());
 			Browser.Driver.manage().deleteCookieNamed(params.get("Cookie Name"));
+			System.out.println("Cookies:"+Browser.Driver.manage().getCookies());			
 		}
 	}
 	
