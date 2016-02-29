657425a05da7b16577e4af07e0775d3c3368d095
chenshu <chenshu@danlu.com>
2016-02-29 21:28:38 +0800

    add wait

--- src/util/DeleteCookie.java
+++ src/util/DeleteCookie.java
@@ -9,13 +9,17 @@ public class DeleteCookie {
 	
 	public static void run(HashMap<String, String> params){
 		if(params.get("Cookie Name") == "ALL"){
+            
+            HashMap<String,String> map1 = new HashMap<String,String>();
+			map1.put("Seconds", "2");
+			Wait.run(map1);
+            
 			System.out.println("Cookie Name == ALL");
 			System.out.println("Before Cookies:"+Browser.Driver.manage().getCookies());
 			Browser.Driver.manage().deleteAllCookies();
 			System.out.println("After Cookies:"+Browser.Driver.manage().getCookies());
 			
-			HashMap<String,String> map1 = new HashMap<String,String>();
-			map1.put("Seconds", "2");
+
 			Wait.run(map1);
 			
 			System.out.println("After Wait:"+Browser.Driver.manage().getCookies());
