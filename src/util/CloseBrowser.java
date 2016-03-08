aae69030fb00c8168e2ea187e63e10d932034a01
liyafei <liyafei@danlu.com>
2016-03-08 17:07:39 +0800

    update close browser .

--- src/util/CloseBrowser.java
+++ src/util/CloseBrowser.java
@@ -5,8 +5,13 @@ import util.Browser;
 public class CloseBrowser {
 
 	public static void run(Object parmas) {
-		if (Browser.Driver != null)
-			Browser.Driver.close();
+        if (Browser.Driver != null)
+            try {
+                Browser.Driver.close();
+                Runtime.getRuntime().exec("taskkill /F /IM IEDriverServer.exe");
+                Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+            } catch (Exception anException) {
+                anException.printStackTrace();
+            }
 	}
-
 }
