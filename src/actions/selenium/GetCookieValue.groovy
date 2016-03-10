886f88bd75d15a460cc72f8fb1504fc8d8adddd3
chenshu <chenshu@danlu.com>
2016-03-10 18:16:09 +0800

    add syso

--- src/actions/selenium/GetCookieValue.groovy
+++ src/actions/selenium/GetCookieValue.groovy
@@ -7,6 +7,7 @@ class GetCookieValue{
   public def run(def params){
     def cookie = Browser.Driver.manage().getCookies().find{it.getName() == params.Name}
     assert cookie != null, "Error cookie not found."
+    System.out.println(cookie.getValue());
     return cookie.getValue()
   }
 }
