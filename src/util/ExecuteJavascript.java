a47223baa2f6fdb2e7a2fe06a29aca65a1ff5c9a
chenshu <chenshu@danlu.com>
2016-03-10 18:48:49 +0800

    add print

--- src/util/ExecuteJavascript.java
+++ src/util/ExecuteJavascript.java
@@ -9,7 +9,9 @@ import util.Browser;
 public class ExecuteJavascript {
 
 	public static void run(HashMap<String, String> params){
+		System.out.println(params.get("Code"));
 		JavascriptExecutor js = (JavascriptExecutor) Browser.Driver;
+		System.out.println(js.toString());
 		js.executeScript(params.get("Code"));
 	}
 	
