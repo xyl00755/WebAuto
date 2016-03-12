3694c62840a99557057366553cb9355e6ab4b741
chenshu <chenshu@danlu.com>
2016-03-12 19:01:07 +0800

    exists ok

--- src/util/Exists.java
+++ src/util/Exists.java
@@ -11,19 +11,14 @@ public class Exists {
 
 	public static void run(HashMap<String, String> params){
 		List<WebElement> elements = Elements.findAll(params, Browser.Driver);
-		System.out.println(params.get("Number of Matches"));
 		String str = params.get("Number of Matches");
 		if( null == str || "".equals(str) || "null".equals(str)){
-			
-			System.out.println("is null");
-			System.out.println("elements.size() = " + elements.size());
-			//assert elements.size() > 0 : "Error element: " + params.get("ID") + " was not found.";
-			
-			Assert.assertEquals(elements.size(),0);
+
+			//assert elements.size() > 0 : "Error element: " + params.get("ID") + " was not found.";	
+			Assert.assertEquals(elements.size()>0,true);
 			
 
 		} else {
-			System.out.println("is not null");
 			//assert elements.size() == Integer.parseInt(params.get("Number of Matches")): "Error element: " + params.get("ID") + " was not found expected number of times: " + str + ".  It was found: " + elements.size() + " times.";
 			Assert.assertEquals(elements.size(),Integer.parseInt(params.get("Number of Matches")));
 		}
