5d6baa0a11a8b68760457d0b9288447740aeb68a
liyafei <liyafei@danlu.com>
2016-05-12 16:33:57 +0800

    add log

--- src/util/Verifies.java
+++ src/util/Verifies.java
@@ -15,28 +15,33 @@ public class Verifies {
 	public static void attribute(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.getAttribute(params.get("Attribute Name")), params.get("Value"));
+        System.out.println("element " + params.get("ID") + " \'s " + params.get("Attribute Name") + "is" + params.get("Value"));
 	}
 
 	// Page Title
 	public static void pageTitle(HashMap<String, String> params) {
 		Assert.assertEquals(Browser.Driver.getTitle(), params.get("Title"));
+        System.out.println("Browser Title is " + params.get("Title"));
 	}
 
 	// Text
 	public static void text(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.getText(), params.get("Text"));
+        System.out.println("element " + params.get("ID") + "\'s Text is" + params.get("Text"));
 	}
 
 	// Current URL
 	public static void currentUrl(HashMap<String, String> params) {
 		Assert.assertEquals(Browser.Driver.getCurrentUrl(), params.get("Value"));
+        System.out.println("Current URL is " + params.get("Value"));
 	}
 
 	// CheckBox
 	public static void checkbox(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.isSelected(), params.get("Is Checked").equals("true"));
+        System.out.println("Checkbox " + params.get("ID") + "is Checked.");
 	}
 
 	// Exist
@@ -57,23 +62,27 @@ public class Verifies {
 	public static void isEnable(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.isEnabled(), true);
+        System.out.println("element " + params.get("ID") + " is enabled.");
 	}
 
 	// Not Enable
 	public static void notEnable(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.isEnabled(), false);
+        System.out.println("element " + params.get("ID") + " is not enabled.");
 	}
 
 	// Visible
 	public static void isVisible(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.isDisplayed(), true);
+        System.out.println("element " + params.get("ID") + " is visible.");
 	}
 
 	// Not Visible
 	public static void notVisible(HashMap<String, String> params) {
 		WebElement element = Elements.find(params, Browser.Driver);
 		Assert.assertEquals(element.isDisplayed(), false);
+        System.out.println("element " + params.get("ID") + " is not visible.");
 	}
 }
