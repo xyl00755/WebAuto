b4f1d295984852b94d60a12afa90eea8cf5c593f
liyafei <liyafei@danlu.com>
2016-05-17 12:00:05 +0800

    add height and width

--- src/util/GetElementSize.java
+++ src/util/GetElementSize.java
@@ -14,4 +14,14 @@ public class GetElementSize {
 		WebElement element = Elements.find(params,Browser.Driver); 
         return element.getSize();
 	}
+    
+    public static int height(HashMap<String, String> params){
+		WebElement element = Elements.find(params,Browser.Driver); 
+        return element.getSize().getHeight();
+	}
+	
+	public static int width(HashMap<String, String> params){
+		WebElement element = Elements.find(params,Browser.Driver); 
+        return element.getSize().getWidth();
+	}
 }
